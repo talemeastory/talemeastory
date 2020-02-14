dc6c5c18dca3759d5c1acb82b9e504031dc2d078
Edwin Perea <edwinperea7@gmail.com>
2020-02-13 23:01:09 -0800

    Move import to da top

--- talemeastory/talemeastory/settings.py
+++ talemeastory/talemeastory/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from .local_settings import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -72,8 +74,6 @@ WSGI_APPLICATION = 'talemeastory.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-from .local_settings import *
 
 
 # Password validation
