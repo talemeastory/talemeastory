adc173f85d86e6877391ca2276cd5089931b9ded
Edwin Perea <edwinperea7@gmail.com>
2020-02-18 22:37:08 -0800

    Add StoryBook model

--- talemeastory/talegate/admin.py
+++ talemeastory/talegate/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import *
+from .models import StoryBook, Story
 
 
 # Register your models here.
+@admin.register(StoryBook)
+class StoryBookAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(Story)
 class StoryAdmin(admin.ModelAdmin):
     pass
--- talemeastory/talegate/models.py
+++ talemeastory/talegate/models.py
@@ -5,7 +5,23 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class StoryBook(models.Model):
+    """The story model is a collection of children passages...which ultimately
+    make up the story :D
+    """
+    title = models.CharField('Story Title', max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(StoryBook, self).save(*args, **kwargs)
+
+
 class Story(models.Model):
+
     title = models.CharField(max_length=30)
     slug = models.SlugField(unique=True, blank=True)
     owner = models.CharField(max_length=30)
@@ -13,8 +29,9 @@ class Story(models.Model):
     date_created = models.DateTimeField(default=datetime.now)
     modified = models.DateTimeField(default=datetime.now)
 
+    def __str__(self):
+        return '{}'.format(self.title)
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Story, self).save(*args, **kwargs)
-
-
