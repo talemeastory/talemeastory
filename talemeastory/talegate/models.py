da33e702162207f4486343a44258867dc8664e3f
sergioavila2720 <sergioavila2720@gmail.com>
2020-02-18 22:58:02 -0800

    Added User

--- talemeastory/talegate/models.py
+++ talemeastory/talegate/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import datetime
 from django.utils.text import slugify
 
@@ -23,8 +24,9 @@ class StoryBook(models.Model):
 class Story(models.Model):
 
     title = models.CharField(max_length=30)
+    storyBook = models.ForeignKey(StoryBook, blank=True, null=True, on_delete=models.SET_NULL)
     slug = models.SlugField(unique=True, blank=True)
-    owner = models.CharField(max_length=30)
+    owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     text = models.CharField(max_length=255)
     date_created = models.DateTimeField(default=datetime.now)
     modified = models.DateTimeField(default=datetime.now)
