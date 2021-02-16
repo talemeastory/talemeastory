4244eac1d75725f3e38a271f88d64439b649be0f
EdwinP7 <edwinperea7@gmail.com>
2021-02-15 23:38:48 -0800

    Add shuffle endpoint for random stories

--- talemeastory/talegate/admin.py
+++ talemeastory/talegate/admin.py
@@ -3,10 +3,12 @@ from .models import *
 
 # Register your models here.
 
+
 @admin.register(Story)
 class StoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in Story._meta.fields]
+
 
 @admin.register(Excerpt)
 class ExcerptAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in Excerpt._meta.fields]
