47f02dbfac6b027487c8a117ab6d2337070fffbd
EdwinP7 <edwinperea7@gmail.com>
2021-02-15 17:21:27 -0800

    Add Excerpt set to Story serializer

--- talemeastory/talegate/serializers.py
+++ talemeastory/talegate/serializers.py
@@ -2,15 +2,17 @@ from rest_framework import serializers
 from .models import *
 
 
-class StorySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Story
-        fields = '__all__'
-
-
 class ExcerptSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Excerpt
+        fields = '__all__'
+
+
+class StorySerializer(serializers.ModelSerializer):
+
+    excerpt_set = ExcerptSerializer(many=True)
+
+    class Meta:
+        model = Story
         fields = '__all__'
